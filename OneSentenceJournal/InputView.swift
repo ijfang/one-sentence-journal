87924922ba9947d130e2e09157457bd48da20628
Isabelle Fang <ijfang19@gmail.com>
2025-09-20 09:29:33 -0400

    added styling for entry page

--- OneSentenceJournal/InputView.swift
+++ OneSentenceJournal/InputView.swift
@@ -12,33 +12,62 @@ struct InputView: View {
     
     var body: some View {
         VStack {
+            Text("One-Sentence Journal")
+                .font(.system(size: 22))
+                .fontWeight(.bold)
+                .foregroundColor(Color(hex: "#5A3E36"))
+                .padding()
+            
             Text("How was your day?")
+                .frame(width: 350, height: 50)
+                .font(.system(size: 22))
+                .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#FFF3C4")))
                 .padding(.bottom, 4)
-                .overlay(RoundedRectangle(cornerRadius: 7)
-                    .stroke(.secondary.opacity(0.5), lineWidth: 1)
+                
+                .overlay(RoundedRectangle(cornerRadius: 30)
+                    .stroke(lineWidth: 10)
+                    .foregroundColor(Color(hex: "#FFDE5C"))
                 )
+                .fontWeight(.bold)
+//            maybe only need to add top padding here
+//                .padding(EdgeInsets(top: 10))
+                
             
             TextField("Enter text",
                       text: $text,
                       prompt: Text("Write a sentence..."),
                       axis: .vertical
                       )
-            .frame(width: 350, height: 200, alignment: .top)
-            .font(.system(size: 20))
-            .overlay(RoundedRectangle(cornerRadius: 7)
-                .stroke(.secondary.opacity(0.5), lineWidth: 1)
-            )
-//            .padding(
-//                EdgeInsets(top:10, leading: 6, bottom: 0, trailing: 6))
+//            .background(Color(hex: "#F9C6D2"))
+                .frame(width: 350, height: 200, alignment: .top)
+                .padding()
+            
+//             this pink should be a little peachier
+                .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#F9C6D2")))
+                .font(.system(size: 22))
+                
+                .overlay(RoundedRectangle(cornerRadius: 50)
+                    .stroke(lineWidth: 10)
+                    .foregroundColor(Color(hex: "#ED8AA0"))
+                
+                )
+                .padding()
 //            .onSubmit {
 //                    validate(name: username)
 //                }
             Button("Save"){}
-                .overlay(RoundedRectangle(cornerRadius: 7)
-                    .stroke(.secondary.opacity(0.5), lineWidth: 1)
+                .frame(width: 175, height: 50)
+                .font(.system(size: 20, design: .rounded))
+                .fontWeight(.bold)
+                .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#F7A96B")))
+                .overlay(RoundedRectangle(cornerRadius: 50)
+                    .stroke(lineWidth: 10)
+                    .foregroundColor(Color(hex: "F5903B"))
                 )
+                .foregroundStyle(Color(hex: "FFE8D5"))
         }
-        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hex: "#FFE8D5"))
         .padding()
         
         
