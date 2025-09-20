402e1e416b77c97e322b7631c72d56c74482b709
Isabelle Fang <ijfang19@gmail.com>
2025-09-19 20:58:51 -0400

    Initial Commit

---
+++ OneSentenceJournal/OneSentenceJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneSentenceJournalApp.swift
+//  OneSentenceJournal
+//
+//  Created by Isabelle Fang on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct OneSentenceJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneSentenceJournalTests/OneSentenceJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneSentenceJournalTests.swift
+//  OneSentenceJournalTests
+//
+//  Created by Isabelle Fang on 9/19/25.
+//
+
+import Testing
+@testable import OneSentenceJournal
+
+struct OneSentenceJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
