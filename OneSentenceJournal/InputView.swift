00cf8358982e90b1decbecad0cfb2d83b6b68385
Isabelle Fang <ijfang19@gmail.com>
2025-09-21 00:00:37 -0400

    Added character count and limit for text field

--- OneSentenceJournal/InputView.swift
+++ OneSentenceJournal/InputView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct InputView: View {
     @State var text = ""
+    let characterLimit = 100
     
     var body: some View {
         VStack {
@@ -23,38 +24,53 @@ struct InputView: View {
                 .font(.system(size: 22))
                 .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#FFF3C4")))
                 .padding(.bottom, 4)
-                
+            
                 .overlay(RoundedRectangle(cornerRadius: 30)
                     .stroke(lineWidth: 10)
                     .foregroundColor(Color(hex: "#FFDE5C"))
                 )
                 .fontWeight(.bold)
-//            maybe only need to add top padding here
-//                .padding(EdgeInsets(top: 10))
-                
+                .foregroundStyle(Color(hex: "#5A3E36"))
             
             TextField("Enter text",
                       text: $text,
                       prompt: Text("Write a sentence..."),
                       axis: .vertical
-                      )
-//            .background(Color(hex: "#F9C6D2"))
-                .frame(width: 350, height: 200, alignment: .top)
-                .padding()
+            )
+            .frame(width: 350, height: 200, alignment: .top)
+            .padding()
             
-//             this pink should be a little peachier
-                .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#F9C6D2")))
-                .font(.system(size: 22))
+            //             this pink should be a little peachier
+            .background(RoundedRectangle(cornerRadius: 50).fill(Color(hex: "#F9C6D2")))
+            .font(.system(size: 22, design: .rounded))
+            
+            .overlay(RoundedRectangle(cornerRadius: 50)
+                .stroke(lineWidth: 10)
+                .foregroundColor(Color(hex: "#ED8AA0"))
+                     
+            )
+            .padding(.top, 20)
+            .onChange(of: text){
+                if text.count > characterLimit {
+                    let limitedText = text.dropLast()
+                    text = String(limitedText)
+                }
+            }
+            
+            //            .onSubmit {
+            //                    validate(name: username)
+            //                }
+            
+            HStack{
+                Spacer()
                 
-                .overlay(RoundedRectangle(cornerRadius: 50)
-                    .stroke(lineWidth: 10)
-                    .foregroundColor(Color(hex: "#ED8AA0"))
-                
-                )
-                .padding()
-//            .onSubmit {
-//                    validate(name: username)
-//                }
+                Text("\(text.count) / \(characterLimit)")
+                    .foregroundColor(Color(hex: "#5A3E36"))
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 30))
+            }
+            
+           
+            
             Button("Save"){}
                 .frame(width: 175, height: 50)
                 .font(.system(size: 20, design: .rounded))
@@ -65,16 +81,18 @@ struct InputView: View {
                     .foregroundColor(Color(hex: "F5903B"))
                 )
                 .foregroundStyle(Color(hex: "FFE8D5"))
+            
+            
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hex: "#FFE8D5"))
         .padding()
         
-        
        
+        
     }
 }
-
 
 #Preview {
     InputView()
